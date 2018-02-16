b43bea8cecf8914a42462c00f657bdbd647388ec
joshua-murphy <josh.murphy11@gmail.com>
2018-02-16 02:06:33 -0700

    add apod url

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,3 @@
 class Photo < ApplicationRecord
+  validates_presence_of [:title, :url, :photo_url, :date]
 end
--- db/migrate/20180215203302_create_photos.rb
+++ db/migrate/20180215203302_create_photos.rb
@@ -1,9 +1,10 @@
 class CreatePhotos < ActiveRecord::Migration[5.1]
   def change
     create_table :photos do |t|
-      t.string :title
-      t.string :url
-      t.date :date
+      t.string :title, null: false
+      t.string :url, null: false
+      t.string :photo_url, null: false
+      t.date :date, null: false
 
       t.timestamps
     end
