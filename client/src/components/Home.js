66aae348fbc056d4d330315da8a20eb1698b9962
joshua-murphy <josh.murphy11@gmail.com>
2018-02-15 19:13:10 -0700

    render photos

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import axios from 'axios';
+import { Container, Header, Image } from 'semantic-ui-react';
 
 class Home extends Component {
+
+  state = { photos: [] }
+
+  componentDidMount() {
+    axios.get('/api/photos')
+      .then( res => this.setState({ photos: res.data }) )
+  }
+
   render() {
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
-    );
+      <Container>
+        { this.state.photos.map( photo => {
+          if( photo.url )
+            return <Image 
+              size="medium" 
+              src={photo.url} 
+              alt="photo.date"
+              href={photo.url} 
+              target="_blank"
+              rel="noopener noreferrer"
+            /> 
+        }) }
+      </Container>
+    )
   }
 }
 
