5cdca04458de8baf8c397296ccb06eaf95c9be6c
joshua-murphy <josh.murphy11@gmail.com>
2018-02-18 05:24:48 -0700

    show description and styling

--- client/src/components/Single.js
+++ client/src/components/Single.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import moment from 'moment';
-import { Container, Header, Image } from 'semantic-ui-react';
+import { Container, Header, Image, Segment } from 'semantic-ui-react';
 
 const Single = ({photo}) => {
 
   return (
-    <Container>
+    <Container as={Segment} raised>
       <Header as="h1" textAlign="center">
         <Header.Content content={photo.title} />
         <Header.Subheader content={moment(photo.date).format("MMM DD, YYYY")} />
       </Header>
       <Image 
         fluid
+        bordered
         href={photo.url} 
         target="_blank"
         rel="noopener noreferrer"
         src={photo.photo_url} 
         alt={photo.title} 
       />
-      <br/>
+      <Header as="p" content={photo.description} style={styles.text} />
     </Container>
   )
+}
+
+const styles = {
+  text: {fontWeight: 120, textAlign: 'center', marginTop: 14}
 }
 
 export default Single;
